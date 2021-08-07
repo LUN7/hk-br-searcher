99e2e3b7b188ab73d0c5a0000eb4fce9f7079739
LUN7 <kwl188836@gmail.com>
2021-08-08 02:09:56 +0800

    feat: add rest api and portal

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-require("./fetch-content");
+const BrDetailScrapper = require("./brn-scrapper");
+const PromiseQueue = require("promise-queue");
+
+const express = require("express");
+const app = express();
+
+const MAX_CONCURRENCY = 1;
+const requestQueue = new PromiseQueue(MAX_CONCURRENCY);
+
+const brDetialScrapper = new BrDetailScrapper();
+
+brDetialScrapper.initialize().then(() => {
+  app.listen(8000, () => {
+    console.log("Server listening on port 8000");
+  });
+});
+
+app.use("/app", express.static("public"));
+app.get("/:brn", async (req, res, next) => {
+  const brnDetail = await requestQueue.add(() =>
+    brDetialScrapper.getBrDetail(req.params.brn)
+  );
+  res.send(brnDetail);
+});
